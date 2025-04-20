6ec8f858506f605f677cd8016d3917022e474b8c
Marcos Gimenez <marcos.ciruja22@gmail.com>
2025-04-20 01:00:44 -0300

    ultimo commit del dia

--- practicas-react/src/Componentes/Contar.jsx
+++ practicas-react/src/Componentes/Contar.jsx
@@ -2,39 +2,37 @@ import React, { useState } from "react";
 
 const Contar = () => {
   const [contador, setContador] = useState(0);
-  const [mensaje, setMensaje] = useState (false)
+  const [mensaje, setMensaje] = useState(false);
 
-    const sumar = () => {
-    setContador(contador + 1)}
-    ;
-    const restar = () => {
-      setContador(contador - 1);
-    };
-    const handleMostrar = () => {
-      setMensaje(true)
-    };
-
-    const handleOcultar = () => {
-      setMensaje(false)
-    }
-    return (
-      <div>
-        {contador}
-        <br />
-        <button onClick={sumar}>Sumar</button>
-        <button onClick={restar}>Restar</button>
-        <br />
-        { !mensaje && 
-          <button onClick={handleMostrar}>Mostrar mensaje</button>}        {
-          mensaje && <div>
-            <h3>Hola Marcos</h3>
-            <button onClick={handleOcultar}>Ocultar Mensaje</button>
-          </div>
-
-        }
-      </div>
-    );
+  const sumar = () => {
+    setContador(contador + 1);
+  };
+  const restar = () => {
+    setContador(contador - 1);
+  };
+  const handleMostrar = () => {
+    setMensaje(true);
   };
 
+  const handleOcultar = () => {
+    setMensaje(false);
+  };
+  return (
+    <div>
+      {contador}
+      <br />
+      <button onClick={sumar}>Sumar</button>
+      <button onClick={restar}>Restar</button>
+      <br />
+      {!mensaje && <button onClick={handleMostrar}>Mostrar mensaje</button>}
+      {mensaje && (
+        <div>
+          <h3>Hola Marcos</h3>
+          <button onClick={handleOcultar}>Ocultar Mensaje</button>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default Contar;
